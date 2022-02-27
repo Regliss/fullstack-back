f2c15c193f1030f727d09b3780caf1268b7929cb
darkregliss <pomart.thomas1@gmail.com>
2022-02-27 22:41:19 +0100

    user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     firstName: {
         type: String,
-        required: true,
+        required: false,
         lowercase: true,
     },
     lastName: {
         type: String,
-        required: true,
+        required: false,
         lowercase: true,
     },
     password: {
